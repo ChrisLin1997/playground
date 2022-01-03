a6b9d96b2e2a34917a860de723932ec7d9ec6898
ChrisLin1997 <i100308326@gmail.com>
2022-01-04 01:02:40 +0800

    Feat: load-ball

--- src/pages/load-ball/index.tsx
+++ src/pages/load-ball/index.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import './style.css'
 
 const LoadBall = () => {
+  const [load, setLoad] = useState(0)
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setLoad(prev => {
+        if (prev + 1 === 100) clearInterval(timerId)
+        return ++prev
+      })
+    }, 50)
+    return () => clearInterval(timerId)
+  }, [])
+
+
   return (
-    <div>
+    <div className="wrap">
       <div className="load-ball">
-        
+        <div className="wave">
+          <div className="before" style={{ marginBottom: load * 1.5 + 'px' }} />
+          <div className="after" style={{ marginBottom: load * 1.5 + 10 + 'px' }} />
+          <div className="percent">{load} %</div>
+        </div>
       </div>
     </div>
   )
